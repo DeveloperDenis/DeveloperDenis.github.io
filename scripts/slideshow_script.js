0c7266fdf5dea7a6efddcf03bb753b2bd956f74d
Denis Levesque <denisllevesque@gmail.com>
2021-02-04 13:33:46 -0700

    Simplified HTML through some JavaScript magic.

--- scripts/slideshow_script.js
+++ scripts/slideshow_script.js
@@ -1,10 +1,36 @@
-var projectIDs = ["project1", "project2", "project3", "project4", "project5", "project6", "project7"];
-var slideIndices = [1, 1, 1, 1, 1, 1, 1]
+var projectIDs = [];
+var slideIndices = [];
 
 window.onload = function()
 {
-	for (var i = 0; i < slideIndices.length; i++)
+	var projects = document.getElementsByClassName("project")
+
+	for (var i = 0; i < projects.length; ++i)
 	{
+		// fill out the arrays needed to perform the slideshow logic
+		projectIDs[i] = "project" + i;
+		slideIndices[i] = 0;
+
+		// dynamically set up slideshow components so that the HTML code can be more modular and simple
+		projects[i].id = projectIDs[i];
+
+		var slides = projects[i].getElementsByClassName("slide");
+		
+		if (slides.length > 1)
+		{
+			// add arrows to slideshow
+			var slideshowContainer = projects[i].getElementsByClassName("slideshow-container")[0];
+			slideshowContainer.innerHTML += "<a class='prev' onclick='nextSlide(" + i + ", -1)'>&#10094;</a>";
+			slideshowContainer.innerHTML += "<a class='next' onclick='nextSlide(" + i + ", 1)'>&#10095;</a>";
+
+			// add dots under slideshow to allow users to change slides
+			var dots = projects[i].getElementsByClassName("dots")[0];
+			for (var j = 0; j < slides.length; ++j)
+			{
+				dots.innerHTML += "<span class='dot' onclick='changeSlide(" + i + ", " + j + ")'></span>"
+			}
+		}
+
 		showSlides(i, slideIndices[i]);
 	}
 }
@@ -27,13 +53,13 @@ function showSlides(slideshowIndex, slideIndex)
 	var dots = document.getElementById(projectIDs[slideshowIndex]).getElementsByClassName("dot");
 
 	slideIndices[slideshowIndex] = slideIndex;
-	if (slideIndex > slides.length)
+	if (slideIndex >= slides.length)
 	{
-		slideIndices[slideshowIndex] = 1;
+		slideIndices[slideshowIndex] = 0;
 	}
-	else if (slideIndex < 1)
+	else if (slideIndex < 0)
 	{
-		slideIndices[slideshowIndex] = slides.length;
+		slideIndices[slideshowIndex] = slides.length - 1;
 	}
 
 	for (i = 0; i < dots.length; i++)
@@ -42,6 +68,6 @@ function showSlides(slideshowIndex, slideIndex)
 		slides[i].style.display = "none";
 	}
 
-	slides[slideIndices[slideshowIndex] - 1].style.display = "block";
-	dots[slideIndices[slideshowIndex] - 1].className += " active";
+	slides[slideIndices[slideshowIndex]].style.display = "block";
+	dots[slideIndices[slideshowIndex]].className += " active";
 }
